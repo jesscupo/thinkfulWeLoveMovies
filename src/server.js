c581a59e011f0c45fef00f65017169dbcf1213e6
Jessica Cupo <jccupo@gmail.com>
2024-02-06 23:03:58 -0500

    fixing ref to db/connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const { PORT = 5001 } = process.env;
 
 const app = require("./app");
-const knex = require("./db/connection");
+const knex = require("./api/db/connection");
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 
