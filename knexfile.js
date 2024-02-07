cc0149fe32ba64d11ab4d5c522614a2c7cc46fb7
Jessica Cupo <jccupo@gmail.com>
2024-02-07 18:49:21 -0500

    fixing dirs in knexfile

--- knexfile.js
+++ knexfile.js
@@ -19,10 +19,10 @@ module.exports = {
     connection: URL,
     pool: { min: 0, max: 5 },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, "src", "api", "db", "migrations"),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, "src", "api", "db", "seeds"),
     },
   },
 
